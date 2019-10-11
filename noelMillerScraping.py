556e4a793f4241f549bfe67b66fbe2fead1a5b99
Claire Murphy <47284954+camurphy408@users.noreply.github.com>
2019-10-11 14:37:37 -0700

    Add files via upload

---
+++ noelMillerScraping.py
@@ -0,0 +1,31 @@
+from pyppeteer import launch
+import asyncio
+
+async def main():
+    browser = await launch(headless=False)
+    page = await browser.newPage()
+    await page.goto('https://twitter.com/thenoelmiller')
+    await asyncio.sleep(1)
+
+# page = requests.get('https://twitter.com/thenoelmiller')
+# tree = html.fromstring(page.content)
+
+    tweets = []
+    tweet_elements = await page.xpath('//ol[@id="stream-items-id"]//div[@class="content"]')
+
+    for elem in tweet_elements:
+        title = await elem.xpath('.//strong[contains(@class, "fullname")]')
+        comments = await elem.xpath('.//button[contains(@class, "js-actionReply")]//span[@class="ProfileTweet-actionCountForPresentation"]')
+        retweets = await elem.xpath('.//button[contains(@class, "js-actionRetweet")]//span[@class="ProfileTweet-actionCountForPresentation"]')
+        likes = await elem.xpath('.//button[contains(@class, "js-actionFavorite")]//span[@class="ProfileTweet-actionCountForPresentation"]')
+
+        name_value = await page.evaluate('(elem) => elem.innerHTML',title[0])
+        comment_count = await page.evaluate('(elem) => elem.innerHTML',comments[0])
+        retweet_count = await page.evaluate('(elem) => elem.innerHTML',retweets[0])
+        like_count = await page.evaluate('(elem) => elem.innerHTML',likes[0])
+
+        tweets.append((name_value, comment_count, retweet_count, like_count))
+
+    print(tweets)
+
+asyncio.get_event_loop().run_until_complete(main())
